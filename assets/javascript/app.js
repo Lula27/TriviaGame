fd1b578494becdf5fd6a83a56036986a83750bf0
Loretta Agyemang <Loretta Agyemang>
2016-12-06 15:03:41 -0500

    chapter 1 added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
-//Prepare for take off! 
+// Prepare for take off! 
 $(document).ready(function(){
 // Create Carousel Effect to present images: jQuery -> https://designmodo.com/responsive-slider/
 
-//Create hover symbol
+// Create hover symbol
 $('#images').css('cursor', 'pointer');
 
-//
+// Load new page with click on image -doesn't seem to be working
+$('#adichie').load("chapter1.html"); 
 }); 
