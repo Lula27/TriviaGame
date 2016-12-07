659c42603beb8ba132a74261b10e19d3791f1bea
Loretta Agyemang <Loretta Agyemang>
2016-12-07 13:08:15 -0500

    linked scripts

---
+++ assets/javascript/c1script.js
@@ -0,0 +1,10 @@
+// Click button to choose answer
+// If correct, print "Correct!" on screen
+// If wrong, print "Incorrect!"
+// Point tracker: how do I connect points on various pages to app.js?
+
+//Q: Which fictional character is not a character in Chimamanda Ngozi Adichie’s "Americanah"? 
+//A: Anansi
+
+// Response for correct answer 
+alert("hi!"); 
